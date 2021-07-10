22c11d64ca206417911aea3fd22fc96ca0329e67
Muhammad Afif <57622408+ad-afif@users.noreply.github.com>
2021-07-10 11:08:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     else:
         while True:
             try:
-                text = input('liyn_language » ')
+                text = input('bloop -> ')
             except EOFError:
                 break
             if text:
